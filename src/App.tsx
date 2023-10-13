294af9e10116bf9b87bcd2de71ca9fb9c0353491
Nathan de Araújo Cunha Lisboa <naclisboa@gmail.com>
2023-10-13 13:15:30 -0300

    duas versoes

--- src/App.tsx
+++ src/App.tsx
@@ -67,3 +67,48 @@ export function App() {
   );
 }
 
+// import { useEffect, useState } from 'react'
+// import axios from 'axios';
+// import { MediaCard } from './MediaCard';
+// import styles from './App.module.css';
+
+// interface PokemonData {
+//   name: string;
+//   url: string;
+// }
+
+// export function App() {
+//   const [pokemonList, setPokemonList] = useState<PokemonData[]>([]);
+//   const [pokemonDetails, setPokemonDetails] = useState<any[]>([]); // Use um tipo adequado para os detalhes
+
+//   useEffect(() => {
+//     axios
+//       .get('https://pokeapi.co/api/v2/pokemon?limit=100') // Exemplo com limite de 10 Pokémon
+//       .then((response) => {
+//         setPokemonList(response.data.results);
+
+//         // Mapeie a lista de Pokémon para buscar os detalhes de cada um
+//         const detailPromises = response.data.results.map((pokemon: PokemonData) =>
+//           axios.get(pokemon.url)
+//         );
+
+//         // Espere que todas as chamadas sejam concluídas
+//         Promise.all(detailPromises)
+//           .then((detailResponses) => {
+//             const details = detailResponses.map((response) => response.data);
+//             setPokemonDetails(details);
+//           })
+//           .catch((error) => console.error('Erro ao buscar detalhes dos Pokémon:', error));
+//       })
+//       .catch((error) => console.error('Erro na chamada à API principal:', error));
+//   }, []);
+
+//   return (
+//     <div className={styles.cards}>
+
+//       {pokemonList.map((pokemon, index) => (
+//         <MediaCard key={index} name={pokemon.name} details={pokemonDetails[index]} />
+//       ))}
+//     </div>
+//   );
+// }
